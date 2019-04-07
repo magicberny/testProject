8bfdeec4f5359c1e4dbf689eb29e7df3045a7bbb
Bernard <test@gmail.com>
2019-04-07 19:58:36 +0100

    initial commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Bernard Gaimbu on 07/04/2019.
+//  Copyright © 2019 Bernard Gaimbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
